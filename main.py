d90d2583009f018828618a8de3b9e74687a43821
testing <goryunova.marta@gmail.com>
2023-03-17 17:53:43 +0200

    hw2

---
+++ main.py
@@ -0,0 +1,15 @@
+import random
+eat = random.randint(0,1)
+class Cat:
+    global eat
+def init(self):
+    if eat == 1:
+        print('Myav!')
+        vubir =input('Eat or dont eat: \n')
+        if vubir == 'Eat':
+            print('Myr')
+        else:
+            print('Myav!')
+    else:
+        print('Myr')
+cat = Cat()
